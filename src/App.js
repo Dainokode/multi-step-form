8314f66b11c3c3ec26bae9a8592cdb990d6b31be
Dainokode <antoniodainok@gmail.com>
2020-10-03 12:59:00 +0200

    signup heading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./styles/main.scss";
 
 const App = () => {
-  return "hello";
+  return (
+    <div className="container">
+      <h1>Sign Up</h1>
+    </div>
+  );
 };
 
 export default App;
